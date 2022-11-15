8b43ed3ed850c8e7b8f27cd0bab30319420e696a
lol <Rahmatfurqon0@gmail.com>
2022-11-15 17:43:45 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css"
+import "../styles/login.module.css"
+import "../styles/landing.module.css"
+import "../styles/home1.module.css"
+import "../styles/profile.module.css"
+import React, {useEffect} from 'react'
+
 
 function MyApp({ Component, pageProps }) {
+  useEffect(()=>{
+    require("bootstrap/dist/js/bootstrap.bundle.min.js")
+  })
   return <Component {...pageProps} />
 }
 
